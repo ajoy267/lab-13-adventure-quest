a09cf5ae58821b37b2af818101a6dc98508d4242
ajoy267 <andrew.joy12@hotmail.com>
2021-10-25 15:21:55 -0700

    wrote my test for generateUser and setUser, wrote the functions and passed the tests

---
+++ app.js
@@ -0,0 +1,11 @@
+import { generateUser, setUser } from './utils.js';
+
+const userForm = document.getElementById('user-form');
+
+userForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(userForm);
+    const userObj = generateUser(formData);
+    setUser(userObj);
+    window.location.replace('./map');
+});
